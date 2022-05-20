9746d21d51ce2dd9828faac88cf3606bb5c92f91
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 08:42:55 +1000

    🎉 Update

--- BuildOrder.py
+++ BuildOrder.py
@@ -16,18 +16,27 @@ def find(elem, dep, seen, solved):
     return out + " " + elem
 
 def graph_to_dict(graph):
+    dic = {}
+
     for pair in graph:
-        pass
+        if str(pair[0]) in dic:
+            dic[str(pair[0])].append(str(pair[1]))
+        else:
+            dic[str(pair[0])] = [str(pair[1])]
+    
+    return dic
 
 if __name__ == "__main__":
-    elem = "a"
+    # elem = "a"
     # dep = {"a": ["b", "e"], "b": ["c", "d", "f"], "d": ["c"], "f": ["e"], "e": ["f"]}
     # dep = {"a": ["b", "e"], "b": ["c", "d", "f"], "d": ["c"], "f": ["e"]}
 
+    elem = "1"
     graph = [[1, 0], [2, 0], [3, 1], [3, 2]]
     dep = graph_to_dict(graph)
+    print(dep)
 
     seen = {}
     solved = {}
 
-    print(find(elem, dep, seen, solved))
+    # print(find(elem, dep, seen, solved))
