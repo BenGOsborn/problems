b663ce4ba855437c7148ac129c2989a54a1082b0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-22 09:39:26 +1000

    🎉 Update

--- Occurances.py
+++ Occurances.py
@@ -1,10 +1,22 @@
-# **** THIS ONE GOT SKIPPED
+def get_substrings(min_len, string):
+    str_len = len(string)
+
+    out = []
+
+    for _len in range(min_len, str_len + 1):
+        for i in range(str_len - _len + 1):
+            temp = ""
+            for k in range(i,i + _len):
+                temp += string[k]
+            out.append(temp)
+        
+    return out
 
 def max_occurances():
     pass
 
 def main():
-    pass
+    print(get_substrings(2, "abcd"))
 
 if __name__ == "__main__":
-    pass
+    main()
