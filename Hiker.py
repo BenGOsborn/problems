d1f92ffd23db002c0307943c7f607f0a9b5b5e91
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-22 07:43:58 +1000

    🎉 Update

--- Hiker.py
+++ Hiker.py
@@ -1,11 +1,22 @@
-def valleys():
-    pass
+def valleys(string):
+    seen = 0
+    height = 0
+
+    started_valley = False
+
+    for char in string:
+        if char == "U":
+            height += 1
+        else:
+            height -= 1
+
+    print(height)
 
 def main():
     inp = "UDDDUDUU"
     steps = len(inp)
 
-    print(valleys())
+    print(valleys(inp))
 
 if __name__ == "__main__":
     main();
