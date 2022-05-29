f5612dfbfa6e44c5e4277638a375448114cbfa4e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-30 08:30:56 +1000

    🎉 Update

--- AlternatingChars.py
+++ AlternatingChars.py
@@ -1,5 +1,17 @@
 def min_removals_for_alt(string):
-    pass
+    if len(string) == 0:
+        return 0
+
+    removals = 0
+
+    prev_char = string[0] 
+    for char in string[1:]:
+        if char == prev_char:
+            removals += 1
+        prev_char = char
+
+    return removals
+
 
 def main():
     print(min_removals_for_alt("AABAAB"))
