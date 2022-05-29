f0c75452e018baf1688a39d98482fffe36aa5a42
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-30 08:17:55 +1000

    🎉 Update

--- Angagram.py
+++ Angagram.py
@@ -1,5 +1,29 @@
+def make_hash(string):
+    dic = {}
+
+    for char in string:
+        if char not in dic:
+            dic[char] = 1
+        else:
+            dic[char] += 1
+
+    return dic
+
+def is_anagram(s1, s2):
+    dic1 = make_hash(s1)
+    dic2 = make_hash(s2)
+
+    for key in dic1:
+        if key in dic2:
+            val = dic2[key]
+
+            dic2[key] = max(val - dic1[key], 0)
+            dic1[key] = max(dic1[key] - val, 0)
+
+    return sum(list(dic1.values()) + list(dic2.values()))
+
 def main():
-    pass
+    print(is_anagram("cde", "abc"))
 
 if __name__ == "__main__":
     main()
