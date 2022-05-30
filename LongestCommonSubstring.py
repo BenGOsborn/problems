96b578d2e4b1e2f8ea4cd91d0370371a0c8b1d25
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-30 13:06:48 +1000

    🎉 Update

--- LongestCommonSubstring.py
+++ LongestCommonSubstring.py
@@ -6,6 +6,21 @@ def longest_common_same(s1, s2):
 
         temp_compared = []
 
+        for j in range(len(s2)):
+            if s1[t] == s2[j]:
+                temp_compared.append(s2[j])
+                t += 1
+
+                if t == len(s1):
+                    break
+            else:
+                t = i
+
+        if len(temp_compared) > len(compared):
+            compared = temp_compared
+
+    return compared
+
 
 
 def main():
