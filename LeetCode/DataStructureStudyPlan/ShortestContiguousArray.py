4f0ae958492509b3d6f68fb1be917c6302a16556
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 16:03:25 +1000

    🎉 Update

--- LeetCode/DataStructureStudyPlan/ShortestContiguousArray.py
+++ LeetCode/DataStructureStudyPlan/ShortestContiguousArray.py
@@ -1,7 +1,25 @@
 tests = [[1,2,2,3,1], [1,2,2,3,1,4,2]]
 
+# Easiest solution
 def shortest_contiguous(nums):
-    pass
+    seen = {}  
+
+    for elem in nums:
+        if elem in seen:
+            seen[elem] += 1
+        else:
+            seen[elem] = 1
+
+    # **** There could be multiple of the same degree that we have to find
+
+    degree = max(seen.values())
+    included = []
+
+    for key in seen.keys():
+        if seen[key] == degree:
+            included.append(key)
+    
+    print(included)
 
 test = tests[0]
 print(shortest_contiguous(test))
