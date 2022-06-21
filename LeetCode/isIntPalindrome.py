73a211a6240f68bab378e7780b6927ceddfba705
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 14:47:17 +1000

    🎉 Update

--- LeetCode/isIntPalindrome.py
+++ LeetCode/isIntPalindrome.py
@@ -3,15 +3,15 @@ tests = [-1, 5, 23, 121, 22, 15351, 15352, 0]
 def is_int_palindrome(x):
     if x > 0:
         temp = x
-        unasembled = []
+        unassembled = []
 
         while temp > 0:
             digit = temp % 10
             temp = temp // 10
-            unasembled.append(digit)
+            unassembled.insert(0, digit)
         
         reassembled = 0
-        for i, digit in enumerate(unasembled):
+        for i, digit in enumerate(unassembled):
             reassembled += digit * 10 ** i
         
         return reassembled == x
