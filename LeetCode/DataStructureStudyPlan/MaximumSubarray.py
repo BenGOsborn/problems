55e6f59ffe6a00dca1598da749315b98474786fc
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 15:56:31 +1000

    🎉 Update

--- LeetCode/DataStructureStudyPlan/MaximumSubarray.py
+++ LeetCode/DataStructureStudyPlan/MaximumSubarray.py
@@ -1,7 +1,19 @@
 tests = [[-2,1,-3,4,-1,2,1,-5,4], [1], [5,4,-1,7,8]]
 
+# Using Kadane's algorithm
 def max_subarray(nums):
-    pass
+    cum_sum = max(nums)
+    current_sum = 0
+
+    for elem in nums:
+        current_sum += elem
+
+        if current_sum < 0:
+            current_sum = 0
+        else:
+            cum_sum = max(cum_sum, current_sum)
+    
+    return cum_sum
 
 test = tests[0]
 print(max_subarray(test))
---
+++ LeetCode/DataStructureStudyPlan/ShortestContiguousArray.py
@@ -0,0 +1,7 @@
+tests = [[1,2,2,3,1], [1,2,2,3,1,4,2]]
+
+def shortest_contiguous(nums):
+    pass
+
+test = tests[0]
+print(shortest_contiguous(test))
