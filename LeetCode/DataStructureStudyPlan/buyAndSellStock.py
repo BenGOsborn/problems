f2708f1d24fc7ff23b1e19c0a7b69c2c6fefe2a1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-26 13:17:23 +1000

    🎉 Update

--- LeetCode/DataStructureStudyPlan/buyAndSellStock.py
+++ LeetCode/DataStructureStudyPlan/buyAndSellStock.py
@@ -1,7 +1,26 @@
-tests = [7, 1, 5, 3, 4, 6]
+tests = [[7, 1, 5, 3, 4, 6], [7, 6, 4, 3, 1]]
+
 
 def max_profit(prices):
-    pass
+    i = 0  # Min
+    j = 0  # Max
 
-test = tests[0]
+    max_profit = 0
+
+    for index in range(len(prices)):
+        if prices[index] < prices[i]:
+            i = index
+            j = index
+        if prices[index] > prices[j]:
+            j = index
+
+        current_min = prices[i]
+        current_max = prices[j]
+
+        max_profit = max(current_max - current_min, max_profit)
+
+    return max_profit
+
+
+test = tests[1]
 print(max_profit(test))
