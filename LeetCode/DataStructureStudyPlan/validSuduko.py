7ce1eed2fbf8c26c878339c1d0c456f710e8c6e7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 08:38:39 +1000

    🎉 Update

--- LeetCode/DataStructureStudyPlan/validSuduko.py
+++ LeetCode/DataStructureStudyPlan/validSuduko.py
@@ -20,14 +20,63 @@ tests = [
         [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 ]
 
-# **** We need to create some sort of lookup table to first off check if the elements we have seen have not been repeated which will then add them to the table to ensure valid
-# **** Final runtime should be O(n^2)
-
-
 class Solution:
+    def check_row(self, matrix, row):
+        seen = {}
+
+        for elem in matrix[row]:
+            if elem in seen and elem != ".":
+                return False
+
+            seen[elem] = True
+        
+        return True
+
+    def check_col(self, matrix, col):
+        seen = {}
+
+        for row in range(len(matrix)):
+            elem = matrix[row][col]
+
+            if elem in seen and elem != ".":
+                return False
+
+            seen[matrix[row][col]] = True
+        
+        return True
+
+    def check_square(self, matrix, row, col, size):
+        seen = {}        
+
+        for i in range(size):
+            for j in range(size):
+                elem = matrix[row + i][col + j]
+
+                if elem in seen and elem != ".":
+                    return False
+
+                seen[elem] = True
+
+        return True
+
     def isValidSudoku(self, board):
-        pass
+        for i in range(len(board)):
+            if not self.check_row(board, i):
+                return False
+        
+        for i in range(len(board[0])):
+            if not self.check_col(board, i):
+                return False
+        
+        size = 3
+        for i in range(len(board) // size):
+            for j in range(len(board[0]) // size):
+                if not self.check_square(board, i * size, j * size, size):
+                    return False
+        
+        return True
 
 
-test = tests[0]
+
+test = tests[1]
 print(Solution().isValidSudoku(test))
