67dc68106cfffc1e2f763852bebafe01c9b89fd4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-26 14:42:12 +1000

    🎉 Update

---
+++ LeetCode/DataStructureStudyPlan/isAnagram.py
@@ -0,0 +1,10 @@
+tests = [("anagram", "nagaram"), ("rat", "car")]
+
+
+class Solution:
+    def isAnagram(self, s, t):
+        pass
+
+
+test = tests[0]
+print(Solution().isAnagram(test[0], test[1]))
