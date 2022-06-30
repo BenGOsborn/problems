7dfd103df0b1b7af290f6cd25040cd915fa84aac
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-30 11:24:49 +1000

    🎉 Update

--- LeetCode/DataStructureStudyPlan/BSTOrderTraversal.py
+++ LeetCode/DataStructureStudyPlan/BSTOrderTraversal.py
@@ -1,6 +1,23 @@
 class Solution:
-    def helper(self):
-        pass
+    def post_order_helper(self, root, positions, depth):
+        if not root:
+            return 
 
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass 
+        if depth in positions:
+            positions[depth].append(root.val)
+        else:
+            positions[depth] = [root.val]
+
+        self.post_order_helper(root.left, positions, depth + 1)
+        self.post_order_helper(root.right, positions, depth + 1)
+
+    def levelOrder(self, root):
+        positions = {}
+
+        self.post_order_helper(root, positions, 1)
+
+        out = []
+        for i in range(len(positions.keys())):
+            out.append(positions[i + 1])
+
+        return out
