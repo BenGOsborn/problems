86e2c6ffdac100969086b09255a5951fa473f3a3
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-15 09:08:35 +1000

    🎉 Update

--- LeetCode/DSSP2/Search2DMatrix.py
+++ LeetCode/DSSP2/Search2DMatrix.py
@@ -12,28 +12,20 @@ class Solution:
             else:
                 return self.binary_search_col(matrix, col, mid + 1, bot, target)
 
-    def binary_search_helper(self, matrix, start, end, target):
+    def searchMatrix(self, matrix, target):
         row_index = len(matrix) - 1
 
-        mid = (start + end) // 2
-
-        if self.binary_search_col(matrix, mid, 0, row_index, target):
-            return True
-        else:
-            if start >= end:
-                return False
-            elif matrix[0][mid] > target:
-                return self.binary_search_helper(matrix, start, mid - 1, target)
-            else:
-                return self.binary_search_helper(matrix, mid + 1, end, target)
-
-
-    def searchMatrix(self, matrix, target):
-        return self.binary_search_helper(matrix, 0, len(matrix[0]) - 1, target)
+        for i in range(len(matrix[0])):
+            if self.binary_search_col(matrix, i, 0, row_index, target):
+                return True
+        
+        return False
 
 tests = [
     ([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5),
     ([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20),
+    ([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 11),
+    ([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 13),
     ([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 7)
     ]
 
