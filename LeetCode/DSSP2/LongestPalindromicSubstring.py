5d33fd62ca35e7f9c3262ef008c9d24a3ff28fc6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-14 21:28:47 +1000

    🎉 Update

--- LeetCode/DSSP2/LongestPalindromicSubstring.py
+++ LeetCode/DSSP2/LongestPalindromicSubstring.py
@@ -1,27 +1,54 @@
+# class SolutionBruteForce:
+#     def is_palindrome(self, s):
+#         stack = []
+
+#         for elem in s:
+#             stack.append(elem)
+
+#         return "".join(stack[::-1]) == s
+
+#     # **** Another alternative I could come up with is identifying each palindrome from the base layer, then trying to build it up in the next increments some way
+
+#     def longestPalindrome(self, s: str) -> str:
+#         str_len = len(s)
+
+#         for i in range(str_len):
+#             curr_len = str_len - i
+
+#             for j in range(str_len - curr_len + 1):
+#                 candidate = s[j:j + curr_len]
+                
+#                 if self.is_palindrome(candidate):
+#                     return candidate
+
 class Solution:
-    def is_palindrome(self, s):
-        stack = []
+    def build_palindrome(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
 
-        for elem in s:
-            stack.append(elem)
-
-        return "".join(stack[::-1]) == s
-
-    # **** Another alternative I could come up with is identifying each palindrome from the base layer, then trying to build it up in the next increments some way
+        return s[l + 1:r]
 
     def longestPalindrome(self, s: str) -> str:
-        str_len = len(s)
+        candidate = ""
 
-        for i in range(str_len):
-            curr_len = str_len - i
+        if len(s) == 1:
+            return s
 
-            for j in range(str_len - curr_len + 1):
-                candidate = s[j:j + curr_len]
-                
-                if self.is_palindrome(candidate):
-                    return candidate
+        for i in range(len(s) - 1):
+            odd = self.build_palindrome(s, i, i)
 
-tests = ["babad", "cbbd", "bb"]
+            if len(odd) > len(candidate):
+                candidate = odd
 
-for test in tests:
+            even = self.build_palindrome(s, i, i + 1)
+
+            if len(even) > len(candidate):
+                candidate = even
+
+        return candidate
+
+tests = ["babad", "cbbd", "bb", "a", "ccc"]
+
+for test in tests[4:]:
     print(Solution().longestPalindrome(test))
