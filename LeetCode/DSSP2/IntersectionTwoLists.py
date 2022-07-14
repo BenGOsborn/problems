7f99d7c979ad247295dc402dd24d5589cf76516b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-14 14:56:19 +1000

    🎉 Update

---
+++ LeetCode/DSSP2/IntersectionTwoLists.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        # **** We just need to do this recursively and then the point where it doesnt intersect will be where it is the same
+
+        pass 
