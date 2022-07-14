3f5aebc159ae64d9ed2698e5b629564406ae8552
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-15 08:38:03 +1000

    🎉 Update

--- LeetCode/DSSP2/Search2DMatrix.py
+++ LeetCode/DSSP2/Search2DMatrix.py
@@ -12,16 +12,24 @@ class Solution:
             else:
                 return self.binary_search_col(matrix, col, mid + 1, bot, target)
 
+    def binary_search_helper(self, matrix, start, end, target):
+        rows = len(matrix)
+
+        mid = (start + end) // 2
+
+        if self.binary_search_col(matrix, mid, 0, rows, target):
+            return True
+        else:
+            if start >= end:
+                return False
+            elif matrix[rows][mid] > target:
+                return self.binary_search_helper(matrix, start, mid - 1, target)
+            else:
+                return self.binary_search_helper(matrix, mid + 1, end, target)
+
+
     def searchMatrix(self, matrix, target):
-        print(matrix, target)
-        print(self.binary_search_col(matrix, 2, 0, 4, 13))
-
-        # **** The way we will do this is by doing a binary search for each column we select,
-        # but we will select columns based off of their range between the minimum of the furthest left column and the max of the furthest right column (using our selected column as the median)
-        # **** This way, we are effectively doing a binary search for our binary searches resulting in (logn) ** 2 time
-
-        pass
-
+        return self.binary_search_helper(matrix, 0, len(matrix[0]), target)
 
 tests = [([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5), ([
     [1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20)]
