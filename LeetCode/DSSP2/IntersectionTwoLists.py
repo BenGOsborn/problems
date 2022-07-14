7e5272e32e396c301e4343d16f9991ff636d53e4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-14 14:57:55 +1000

    🎉 Update

--- LeetCode/DSSP2/IntersectionTwoLists.py
+++ LeetCode/DSSP2/IntersectionTwoLists.py
@@ -7,5 +7,6 @@
 class Solution:
     def getIntersectionNode(self, headA, headB):
         # **** We just need to do this recursively and then the point where it doesnt intersect will be where it is the same
+        # **** We will just assume that if the next one is correct then the previous one must also be correct and we will keep doing this until ... ?
 
         pass 
