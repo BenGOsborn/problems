0bde48dc7b224c11125506e61a8a73dae6193101
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-14 14:47:31 +1000

    🎉 Update

--- LeetCode/DSSP2/LongestPalindrome.py
+++ LeetCode/DSSP2/LongestPalindrome.py
@@ -1,3 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        pass
+        mapping = {}
+
+        for char in s:
+            if char not in mapping:
+                mapping[char] = 1
+            else:
+                mapping[char] += 1
+
+        seen_odd = False
+        longest = 0
+        for key, value in mapping.items():
+            if value % 2 != 0:
+                if seen_odd:
+                    longest += value - 1
+                else:
+                    longest += value
+                    seen_odd = True
+            else:
+                longest += value
+
+        return longest
+
+tests = ["abccccdd", "a"]
+
+for test in tests:
+    print(Solution().longestPalindrome(test))
