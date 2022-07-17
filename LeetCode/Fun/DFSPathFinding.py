8e09220e67c586d8b07d43f7a7eb8955b9810741
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-18 08:50:36 +1000

    🎉 Update

--- LeetCode/Fun/DFSPathFinding.py
+++ LeetCode/Fun/DFSPathFinding.py
@@ -1,15 +1,35 @@
+def dfs(source, graph, target, path, seen):
+    if source in seen:
+        return False
+
+    seen[source] = True
+
+    path.append(source)
+
+    for elem in graph[source]:
+        if elem == target:
+            path.append(target)
+            return True
+
+        if dfs(elem, graph, target, path, seen):
+            return True
+
+    path.pop(-1)
+
+    return False
+
 def main():
     start = "a"
     end = "f"
     graph = {"a": {"b", "c"}, "b": {"c", "d"}, "c": {"e"}, "e": {"d"}, "d": {"f"}, "f": set()}
 
     seen = {}
-    stack = [start]
 
-    while len(stack) > 0:
-        pass
+    path = []
 
-    print(stack)
+    dfs(start, graph, end, path, seen)
+
+    print(path)
 
 if __name__ == "__main__":
     main()
