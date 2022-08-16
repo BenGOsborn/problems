d0b4c6ab6b952894b2a5b60d7816fefbcacfa3ff
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-08-16 14:02:09 +1000

    🎉 Update

--- ccti/arrays-strings/PalindromePermutation.py
+++ ccti/arrays-strings/PalindromePermutation.py
@@ -1,9 +1,23 @@
 def is_palindrome_permutation(string):
-    pass
+    mapping = {}
+    for char in string:
+        if char not in mapping:
+            mapping[char] = 1
+        else:
+            mapping[char] += 1
+
+    odd_flag = False
+    for key in mapping.keys():
+        if mapping[key] % 2 != 0:
+            if odd_flag == True:
+                return False
+            odd_flag = True
+
+    return True
 
 # Assume no spaces
 
-tests = ["tacocat", "arceacr"]
+tests = ["tacocat", "arceacr", "cat"]
 
 for test in tests:
     print(is_palindrome_permutation(test))
