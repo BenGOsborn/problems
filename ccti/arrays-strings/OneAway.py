b567b8df0903c5eda1e95ba982952d3423230587
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-08-21 09:09:27 +1000

    🎉 Update

--- ccti/arrays-strings/OneAway.py
+++ ccti/arrays-strings/OneAway.py
@@ -2,17 +2,36 @@ def one_away(string1, string2):
     if string1 == string2:
         return True
 
-    if abs(len(string1) - len(string2)) > 2:
+    diff = abs(len(string1) - len(string2))
+
+    if diff > 1:
         return False
 
-    # We need to check the append case
-    # We need to check the removal case
-    # We need to check the replace case
+    min_size = min(len(string1), len(string2))
+
+    if diff == 1:
+        for i in range(min_size):
+            if string1[i] != string2[i]:
+                if len(string1) > len(string2):
+                    cmpre = string1[:i] + string1[i + 1:]
+                    return cmpre == string2
+                else:
+                    cmpre = string2[:i] + string2[i + 1:]
+                    return cmpre == string1
+
+        return True
+
+    if diff == 0:
+        for i in range(min_size):
+            if string1[i] != string2[i]:
+                cmpre = string1[:i] + string2[i] + string1[i + 1:]
+                return cmpre == string2
 
 # We need to check if one (or zero) edit can be made to the strings to make them equal
 # - If they are equal they are valid
 # - If they are more than two in length away it is invalid
-# - If we can construct one out of the other and only have to make one change, we know we have found the result
+# - If they have a difference of one between them, we simply just need to find where the differ, remove the point, then check if they are equal or not. If no, it is invalid
+# - If they have a difference of zero between them, we just need to replace one character, and if replacing that one character does not work it is invalid
 
 
 tests = [("paie", "pie"), ("pales", "pale"),
