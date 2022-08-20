cb768835c6f14ac51db64a51918a6b008ff3c3a9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-08-21 08:50:05 +1000

    🎉 Update

---
+++ ccti/arrays-strings/ZeroMatrix.py
@@ -0,0 +1,32 @@
+def zero_row_col(array, row, col):
+    for i in range(len(array[row])):
+        array[row][i] = 0
+
+    for i in range(len(array)):
+        array[i][col] = 0
+
+
+def zero_matrix(array):
+    seen_rows = {}
+    seen_cols = {}
+
+    for i in range(len(array)):
+        if i in seen_rows:
+            continue
+
+        for j in range(len(array[i])):
+            if j in seen_cols:
+                continue
+
+            if array[i][j] == 0:
+                zero_row_col(array, i, j)
+                seen_rows[i] = True
+                seen_cols[j] = True
+                break
+
+
+tests = [[[1, 2, 0, 4], [1, 0, 2, 3], [2, 5, 1, 1], [0, 2, 2, 1]]]
+
+for test in tests:
+    zero_matrix(test)
+    print(test)
