907ba7aebfdb84cfe4d12f0ac41f219f674914ae
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-14 22:57:10 +1000

    🎉 Update

---
+++ leet-code/fun/WordBreak.py
@@ -0,0 +1,6 @@
+# Algorithm
+
+# - Take all of the word dictionaries and add them to a single Trie structure
+# - Loop through the characters in the string and compare them to the trie structure. If we encounter an element that is not at the next node in the trie structure, return an error.
+# - If we get to an element that is at the end of the trie structure, go to the beginning
+# - When we terminate, if we terminate at the end of a trie loop, we will return true, otherwise we will return false
