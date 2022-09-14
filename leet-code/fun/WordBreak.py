8c7649d5f48b660470558fea425eb4578c11f9c8
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-14 22:59:54 +1000

    🎉 Update

--- leet-code/fun/WordBreak.py
+++ leet-code/fun/WordBreak.py
@@ -4,3 +4,5 @@
 # - Loop through the characters in the string and compare them to the trie structure. If we encounter an element that is not at the next node in the trie structure, return an error.
 # - If we get to an element that is at the end of the trie structure, go to the beginning
 # - When we terminate, if we terminate at the end of a trie loop, we will return true, otherwise we will return false
+
+# Time complexity: O(n + k * m) (where k is the maximum size of a word in the dict and m is the size of the dict) | Space complexity: O(k * m)
