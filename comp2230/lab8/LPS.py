84336701ad611daf75d9a369365837b76b09c9c5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-14 15:10:57 +1000

    🎉 Update

---
+++ comp2230/lab8/LPS.py
@@ -0,0 +1,32 @@
+# https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+def computeLPSArray(pat, lps):
+    ln = 0
+
+    lps[0]
+    i = 1
+
+    while i < len(pat):
+        if pat[i] == pat[ln]:
+            ln += 1
+            lps[i] = ln
+            i += 1
+        else:
+            if ln != 0:
+                ln = lps[ln-1]
+
+            else:
+                lps[i] = 0
+                i += 1
+
+
+pat = "pappar"
+lps = [0] * len(pat)
+
+computeLPSArray(pat, lps)
+print(lps)
+
+pat = "ababbcabab"
+lps = [0] * len(pat)
+
+computeLPSArray(pat, lps)
+print(lps)
