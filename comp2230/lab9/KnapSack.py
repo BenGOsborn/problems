6701593bedcc9e20ef592d0db17affc55637cdf6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-21 11:01:48 +1000

    🎉 Update

--- comp2230/lab9/KnapSack.py
+++ comp2230/lab9/KnapSack.py
@@ -1,6 +1,24 @@
-def knapsack(current, available, weight):
+def encode():
     pass
+
+
+def knapsack(current_value, current_weight, remaining, max_weight):
+    current_max = current_value
+
+    for i, elem in enumerate(remaining):
+        new_weight = current_weight + elem[0]
+        if new_weight > max_weight:
+            continue
+
+        out = knapsack(current_value + elem[1], new_weight,
+                       remaining[:i] + remaining[i+1:], max_weight)
+
+        if out > current_max:
+            current_max = out
+
+    return current_max
 
 
 available = [(2, 5), (3, 8), (1, 7), (2, 15)]
 weight = 5
+print(knapsack(0, 0, available, weight))
