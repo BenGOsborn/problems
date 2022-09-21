3de809dc00694942c907d1dda5375b52d5715098
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-21 12:53:47 +1000

    🎉 Update

---
+++ comp2230/lab9/GraphNegativeCycle.py
@@ -0,0 +1,44 @@
+# Algorithm
+# - Record the weight from each node to the next node inside of the vertex
+# - If we get to a cycle, subtract the cumulative weight from the last node to the original vertex entry weight - if it is negative, we have a negative cycle, else continue
+# - If we can't find anything, return false
+
+from math import inf
+
+
+def graph_negative_cycle_rec(entry, prev_weight, graph, cum_weights):
+    if entry in cum_weights:
+        if prev_weight - cum_weights[entry] < 0:
+            print(entry, prev_weight, cum_weights)
+            return True
+        return False
+
+    cum_weights[entry] = prev_weight
+
+    for elem in graph[entry]:
+        out = graph_negative_cycle_rec(
+            elem[1], prev_weight + elem[0], graph, cum_weights)
+        if out:
+            return True
+
+    return False
+
+
+def graph_negative_cycle(graph):
+    cum_weights = {}
+
+    for elem in graph:
+        out = graph_negative_cycle_rec(elem, 0, graph, cum_weights)
+        if out:
+            return True
+
+    return False
+
+
+tests = [
+    {"a": [(2, "b"), (4, "c")], "b": [(4, "a")], "c": []},
+    {"a": [(2, "b"), (4, "c")], "b": [(-4, "a")], "c": []}
+]
+
+for test in tests:
+    print(graph_negative_cycle(test))
