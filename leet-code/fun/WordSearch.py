b221056e7bbb5c9e0fd14d94300e6bae30475d77
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-22 10:06:29 +1000

    🎉 Update

--- leet-code/fun/WordSearch.py
+++ leet-code/fun/WordSearch.py
@@ -1,11 +1,37 @@
 class Solution:
-    def dfs():
-        pass
+    def encode(self, i, j):
+        return f"{i},{j}"
+
+    def dfs(self, board, word, i, j, n, seen):
+        encoded = self.encode(i, j)
+        if encoded in seen:
+            return False
+
+        seen[encoded] = True
+
+        if n == len(word):
+            return True
+        if board[i][j] != word[n]:
+            return False
+
+        if i > 0 and i < len(board) and self.dfs(board, word, i - 1, j, n + 1, seen):
+            return True
+        if i >= 0 and i < len(board) - 1 and self.dfs(board, word, i + 1, j, n + 1, seen):
+            return True
+        if j > 0 and j < len(board[0]) and self.dfs(board, word, i, j - 1, n + 1, seen):
+            return True
+        if j >= 0 and j < len(board[0]) - 1 and self.dfs(board, word, i, j + 1, n + 1, seen):
+            return True
+
+        return False
 
     def exist(self, board, word):
         for i in range(len(board)):
             for j in range(len(board[i])):
-                pass
+                if self.dfs(board, word, i, j, 0, {}):
+                    return True
+
+        return False
 
 
 tests = [
