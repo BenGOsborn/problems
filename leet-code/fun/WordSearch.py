00cf49317fddb1f360340047aaf8cb4146b11120
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-22 11:10:54 +1000

    🎉 Update

--- leet-code/fun/WordSearch.py
+++ leet-code/fun/WordSearch.py
@@ -1,13 +1,9 @@
 class Solution:
-    def encode(self, i, j):
-        return f"{i},{j}"
+    def dfs(self, board, word, i, j, n):
+        if not (i >= 0 and i < len(board) and j >= 0 and j < len(board[0])):
+            return False
 
-    def dfs(self, board, word, i, j, n, seen):
-        # **** Now I need to reimplement the cycle detection system for a particular path
-        # **** There should be a better way of doing this with only one batch of memory...
-
-        encoded = self.encode(i, j)
-        if encoded in seen:
+        if board[i][j] == "#":
             return False
 
         if board[i][j] != word[n]:
@@ -15,34 +11,34 @@ class Solution:
         if n == len(word) - 1:
             return True
 
-        seen[encoded] = True
+        temp = board[i][j]
+        board[i][j] = "#"
 
-        if i > 0 and i < len(board) and self.dfs(board, word, i - 1, j, n + 1, seen.copy()):
-            return True
-        if i >= 0 and i < len(board) - 1 and self.dfs(board, word, i + 1, j, n + 1, seen.copy()):
-            return True
-        if j > 0 and j < len(board[0]) and self.dfs(board, word, i, j - 1, n + 1, seen.copy()):
-            return True
-        if j >= 0 and j < len(board[0]) - 1 and self.dfs(board, word, i, j + 1, n + 1, seen.copy()):
-            return True
+        for x, y in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+            if self.dfs(board, word, i + y, j + x, n + 1):
+                return True
+
+        board[i][j] = temp
 
         return False
 
     def exist(self, board, word):
         for i in range(len(board)):
             for j in range(len(board[i])):
-                if self.dfs(board, word, i, j, 0, {}):
+                if self.dfs(board, word, i, j, 0):
                     return True
 
         return False
 
 
 tests = [
-    ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE"),
-    ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB"),
-    ([["a"]], "a"),
-    ([["a", "b"], ["c", "d"]], "cdba"),
-    ([["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]], "ABCESEEEFS")
+    # ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE"),
+    # ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB"),
+    # ([["a"]], "a"),
+    # ([["a", "b"], ["c", "d"]], "cdba"),
+    # ([["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]], "ABCESEEEFS"),
+    ([["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], [
+     "A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "B"], ["A", "A", "A", "A", "B", "A"]], "AAAAAAAAAAAAABB")
 ]
 
 for test in tests:
