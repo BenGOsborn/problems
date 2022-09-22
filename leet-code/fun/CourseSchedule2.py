7dd9eb98658c359428fd595d1773c0fa9eac5734
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-23 09:40:45 +1000

    🎉 Update

--- leet-code/fun/CourseSchedule2.py
+++ leet-code/fun/CourseSchedule2.py
@@ -1,7 +1,21 @@
 class Solution:
-    def topological_sort(self, start, graph, current, seen):
-        # **** We need to consider the edge case where we have a cycle
-        pass
+    def topological_sort(self, start, graph, marked, out):
+        if start in marked:
+            if marked[start]:
+                return True
+            return False
+
+        marked[start] = False
+
+        if start in graph:
+            for elem in graph[start]:
+                if not self.topological_sort(elem, graph, marked, out):
+                    return False
+
+        marked[start] = True
+        out.append(start)
+
+        return True
 
     def findOrder(self, numCourses, prerequisites):
         graph = {}
@@ -11,8 +25,13 @@ class Solution:
             else:
                 graph[elem[0]] = [elem[1]]
 
-        for course_num in range(numCourses):
-            pass
+        out = []
+        marked = {}
+        for elem in range(numCourses):
+            if elem not in marked and not self.topological_sort(elem, graph, marked, out):
+                return []
+
+        return out
 
 
 tests = [
