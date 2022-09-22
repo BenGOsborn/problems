64aeb925ef0d98be66be2a5c8ab49f5a6eeb5344
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-23 08:15:47 +1000

    🎉 Update

---
+++ leet-code/fun/JumpGame.py
@@ -0,0 +1,28 @@
+class Solution:
+    def can_jump_h(self, nums, i, cache):
+        if i in cache:
+            return cache[i]
+
+        if i >= len(nums):
+            cache[i] = False
+            return False
+        if i == len(nums) - 1:
+            cache[i] = True
+            return True
+
+        for j in range(1, min(nums[i] + 1, len(nums) - i)):
+            if self.can_jump_h(nums, i + j, cache):
+                cache[i] = True
+                return True
+
+        cache[i] = False
+        return False
+
+    def canJump(self, nums):
+        return self.can_jump_h(nums, 0, {})
+
+
+tests = [[2, 3, 1, 1, 4], [3, 2, 1, 0, 4]]
+
+for test in tests:
+    print(Solution().canJump(test))
