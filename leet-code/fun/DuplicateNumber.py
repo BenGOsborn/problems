96864498bde0bd67e5989967a48a401cbc24c26a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-23 10:28:47 +1000

    🎉 Update

---
+++ leet-code/fun/DuplicateNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums):
+        pass
+
+
+tests = [[1, 3, 4, 2, 2], [3, 1, 3, 4, 2]]
+
+for test in tests:
+    print(Solution().findDuplicate(test))
--- leet-code/fun/Trie.py
+++ leet-code/fun/Trie.py
@@ -1,12 +1,52 @@
+class Node:
+    def __init__(self):
+        self.map = {}
+        self.end = False
+
+
 class Trie:
     def __init__(self):
-        pass
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        pass
+        current = self.root
+
+        for char in word:
+            if char not in current.map:
+                current.map[char] = Node()
+            current = current.map[char]
+
+        current.end = True
 
     def search(self, word: str) -> bool:
-        pass
+        current = self.root
+
+        for char in word:
+            if char not in current.map:
+                return False
+            current = current.map[char]
+
+        return current.end
 
     def startsWith(self, prefix: str) -> bool:
-        pass
+        current = self.root
+
+        for char in prefix:
+            if char not in current.map:
+                return False
+            current = current.map[char]
+
+        return True
+
+
+trie = Trie()
+
+trie.insert("apple")
+
+print(trie.search("apple"))  # return True
+print(trie.search("app"))  # return False
+print(trie.startsWith("app"))  # return True
+
+trie.insert("app")
+
+print(trie.search("app"))  # return True
