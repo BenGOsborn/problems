81517077b77dc8e825a562ff6bcd5597ec4eb973
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-28 08:12:34 +1000

    🎉 Update

--- leet-code/arrays/SearchRotatedSorted.py
+++ leet-code/arrays/SearchRotatedSorted.py
@@ -10,29 +10,41 @@ class Solution:
 
         mid = (i + j) // 2
 
-        # **** Use a combination of (if mid greater than low and less than high for example to do the casing) - https://leetcode.com/problems/search-in-rotated-sorted-array/
-
-        if nums[mid] == target:
+        if target == nums[mid]:
             return mid
 
-        if nums[i] < nums[j]:
-            if target > mid:
+        if nums[mid] >= nums[i] and nums[j] >= nums[mid]:  # i < mid, mid < j
+            if target > nums[mid]:
                 return self.bin_search(nums, target, mid + 1, j)
             return self.bin_search(nums, target, i, mid - 1)
+        elif nums[mid] > nums[i] and nums[mid] > nums[j]:  # i < mid, mid > j
+            if target > nums[mid]:
+                return self.bin_search(nums, target, mid + 1, j)
 
-        if target < nums[mid]:
+            if target >= nums[i]:
+                return self.bin_search(nums, target, i, mid - 1)
+
             return self.bin_search(nums, target, mid + 1, j)
-        return self.bin_search(nums, target, i, mid - 1)
+        elif nums[mid] < nums[i] and nums[j] > nums[mid]:  # i > mid, mid < j
+            if target < nums[mid]:
+                return self.bin_search(nums, target, i, mid - 1)
+
+            if target <= nums[j]:
+                return self.bin_search(nums, target, mid + 1, j)
+
+            return self.bin_search(nums, target, i, mid - 1)
 
     def search(self, nums, target):
         return self.bin_search(nums, target, 0, len(nums) - 1)
 
 
 tests = [
-    # (([4, 5, 6, 7, 0, 1, 2], 0), 4),
-    # (([4, 5, 6, 7, 0, 1, 2], 3), -1),
-    # (([1], 0), -1),
-    (([4, 5, 6, 7, 8, 1, 2, 3], 8), 4)
+    (([4, 5, 6, 7, 0, 1, 2], 0), 4),
+    (([4, 5, 6, 7, 0, 1, 2], 3), -1),
+    (([1], 0), -1),
+    (([4, 5, 6, 7, 8, 1, 2, 3], 8), 4),
+    (([6, 7, 8, 1, 2, 3, 4, 5], 8), 2),
+    (([1, 2, 3], 8), -1)
 ]
 
 for test in tests:
