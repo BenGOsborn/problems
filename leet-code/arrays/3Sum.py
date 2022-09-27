cb378b7859591cc0acd545510a976e9a80fa6328
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-27 15:29:55 +1000

    🎉 Update

--- leet-code/arrays/3Sum.py
+++ leet-code/arrays/3Sum.py
@@ -1,6 +1,13 @@
+# Algorithm
+# - Sort the array O(nlogn)
+# - For all n elements in the array, use this to get the new target value, and then run the two sum for the rest of the array
+# - To not get duplicates, maybe we just keep an array of elements that we already have ?
+
 class Solution:
     def threeSum(self, nums):
-        pass
+        target = 0
+
+        nums.sort()
 
 
 tests = [
@@ -10,6 +17,6 @@ tests = [
 ]
 
 for test in tests:
-    print(Solution().threeSum(*test[0]))
+    print(Solution().threeSum(test[0]))
     print(test[1])
     print()
