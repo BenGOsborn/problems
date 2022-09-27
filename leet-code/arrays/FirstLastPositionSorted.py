591d94eaf9220e92067f14f1bba144ca8636ef64
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-27 10:28:50 +1000

    🎉 Update

---
+++ leet-code/arrays/FirstLastPositionSorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchRange(self, nums, target):
+        pass
+
+
+tests = [
+    (([5, 7, 7, 8, 8, 10], 8), [3, 4]),
+    (([5, 7, 7, 8, 8, 10], 6), [-1, -1]),
+    (([], 0), [-1, -1])
+]
+
+for test in tests:
+    print(Solution().searchRange(*test[0]))
+    print(str(test[1]) + "\n")
