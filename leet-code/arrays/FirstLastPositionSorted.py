e1a9aeadb1419b8039fcba374275afd392a97a7b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-27 10:56:12 +1000

    🎉 Update

--- leet-code/arrays/FirstLastPositionSorted.py
+++ leet-code/arrays/FirstLastPositionSorted.py
@@ -1,15 +1,42 @@
-# Algorithm
-# - We are going to do a binary search to find both the start and the end of the list - but how ?
-
 class Solution:
+    def bin_search_first(self, nums, target, i, j):
+        if i > j:
+            return -1
+
+        mid = (i + j) // 2
+
+        if nums[mid] == target and (mid == 0 or nums[mid - 1] < target):
+            return mid
+        elif target > nums[mid]:
+            return self.bin_search_first(nums, target, mid + 1, j)
+        else:
+            return self.bin_search_first(nums, target, i, mid - 1)
+
+    def bin_search_last(self, nums, target, i, j):
+        if i > j:
+            return -1
+
+        mid = (i + j) // 2
+
+        if nums[mid] == target and (mid == len(nums) - 1 or nums[mid + 1] > target):
+            return mid
+        elif target >= nums[mid]:
+            return self.bin_search_last(nums, target, mid + 1, j)
+        else:
+            return self.bin_search_last(nums, target, i, mid - 1)
+
     def searchRange(self, nums, target):
-        pass
+        first = self.bin_search_first(nums, target, 0, len(nums) - 1)
+        if first == -1:
+            return [-1, -1]
+        return [first, self.bin_search_last(nums, target, 0, len(nums) - 1)]
 
 
 tests = [
     (([5, 7, 7, 8, 8, 10], 8), [3, 4]),
     (([5, 7, 7, 8, 8, 10], 6), [-1, -1]),
-    (([], 0), [-1, -1])
+    (([], 0), [-1, -1]),
+    (([1, 1, 2], 1), [0, 1])
 ]
 
 for test in tests:
