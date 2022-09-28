357a9753c0864054e605648dc7459c48363499df
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-09-28 20:04:55 +1000

    🎉 Update

---
+++ leet-code/strings/LongestSubstringNoRepeat.py
@@ -0,0 +1,8 @@
+# Algorithm
+# - When we iterate through, we will keep count of the max substring that we encounter, as well as when the subarray started
+# - We will iterate over the string and will keep a count of the indexes of our seen elements
+# - I can check if the element is a duplicate access by looking at where the index occurs - we can overwrite the previous index with a new one once we resee it
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
