433582d67d43aa94f552a8f058018f995afeb6bf
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-10-01 21:26:42 +1000

    🎉 Update

--- leet-code/dp/BestStockTime2.py
+++ leet-code/dp/BestStockTime2.py
@@ -1,6 +1,27 @@
+# Algorithm
+# - We will define the subproblems as getting the maximum profit between any two elements
+# - We will work backwards, and will store the maximum result for each as well as the total maximum amount obtainable (which will begin at zero)
+# - We will only allow this to be updated if the element in front of it is greater than the element we are previously on
+
 class Solution:
+    def max_profit(self, prices, i, cache):
+        if i == len(prices) - 1:
+            cache[i] = 0
+            return cache[i]
+
+        mx = 0
+
+        for j in range(i + 1, len(prices)):
+            elem = cache[j]
+            if elem == -1:
+                elem = self.max_profit(prices, j, cache)
+            mx = max(mx, prices[j] - prices[i] + elem, elem)
+
+        cache[i] = mx
+        return cache[i]
+
     def maxProfit(self, pricesList):
-        pass
+        return self.max_profit(pricesList, 0, [-1] * len(pricesList))
 
 
 tests = [
