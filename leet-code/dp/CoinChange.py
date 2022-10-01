3a5606b271221d874186e499eecc83d927782d9c
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-10-01 21:55:13 +1000

    🎉 Update

--- leet-code/dp/CoinChange.py
+++ leet-code/dp/CoinChange.py
@@ -1,3 +1,13 @@
 class Solution:
     def coinChange(self, coins, amount):
         pass
+
+
+tests = [
+    (([1, 2, 5], 11), 3),
+    (([2], 3), -1),
+    (([1], 0), 0)
+]
+
+for test in tests:
+    print(Solution().coinChange(*test[0]), test[1])
