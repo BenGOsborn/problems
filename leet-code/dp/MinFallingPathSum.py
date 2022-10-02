fb2399461a192ab8a1c643c62cb6bf88455282f5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-10-03 08:26:11 +1100

    🎉 Update

--- leet-code/dp/MinFallingPathSum.py
+++ leet-code/dp/MinFallingPathSum.py
@@ -1,6 +1,31 @@
+# Algorithm
+# - Use a bottom up approach to generate the minimum path sum of the elements at every level
+# - From there, we just need to return the minimum path sum for the top row and then return it
+
 class Solution:
     def minFallingPathSum(self, matrix):
-        pass
+        cache = []
+        for i in range(len(matrix)):
+            temp = []
+            for j in range(len(matrix[0])):
+                temp.append(None)
+            cache.append(temp)
+
+        for i in range(len(matrix) - 1, -1, -1):
+            for j in range(len(matrix[0])):
+                if i == len(matrix) - 1:
+                    cache[i][j] = matrix[i][j]
+                else:
+                    cache[i][j] = matrix[i][j] + cache[i + 1][j]
+
+                    if j > 0:
+                        cache[i][j] = min(
+                            cache[i][j], matrix[i][j] + cache[i + 1][j - 1])
+                    if j < len(matrix[0]) - 1:
+                        cache[i][j] = min(
+                            cache[i][j], matrix[i][j] + cache[i + 1][j + 1])
+
+        return min(cache[0])
 
 
 tests = [
