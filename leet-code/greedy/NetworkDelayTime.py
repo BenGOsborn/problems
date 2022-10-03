1c2f0e3c14232a6171e02a0aa0e4286ceb5b9e29
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-10-03 11:29:21 +1100

    🎉 Update

--- leet-code/greedy/NetworkDelayTime.py
+++ leet-code/greedy/NetworkDelayTime.py
@@ -1,6 +1,26 @@
+from queue import PriorityQueue
+
+# Algorithm
+# - Implement djikstras to find the shortest path to every node
+# - Return the minimum of the shortest paths (including -1)
+
+
 class Solution:
     def networkDelayTime(self, times, n, k):
-        pass
+        cache = [-1] * n  # Seen elements are not -1
+
+        graph = {}
+        for time in times:
+            if time[0] not in graph:
+                graph[time[0]] = PriorityQueue()
+
+            graph[time[0]].put((time[2], time))
+
+        print(graph)
+
+        # **** We just need to explore every node, and then update the previous node with the new value if it has already been taken
+
+        return min(cache)
 
 
 tests = [
