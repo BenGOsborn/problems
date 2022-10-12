ee7c9919db6f00f8a7fd27921c3f53765797c4b7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-10-13 08:50:29 +1100

    🎉 Update

---
+++ comp2230/lab8/Shift.py
@@ -0,0 +1,33 @@
+import collections
+
+
+def kmp_shift(pattern):
+    shift = collections.defaultdict(int)
+
+    shift[-1] = 1
+    shift[0] = 1
+
+    i = 1
+    j = 0
+
+    while i + j < len(pattern):
+        if pattern[i + j] == pattern[j]:
+            shift[i + j] = i
+            j += 1
+        else:
+            if j == 0:
+                shift[i] = i + 1
+            i = i + shift[j - 1]
+            j = max(j - shift[j - 1], 0)
+
+        print(i, j, shift)
+
+    return shift
+
+
+tests = [
+    ("ababbcababa",)
+]
+
+for test in tests:
+    print(kmp_shift(test[0]))
